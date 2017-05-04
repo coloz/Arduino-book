9db98ff9f26776e8f7af53d447d1a9e2981d8282
laifeilim <50793655@qq.com>
2017-05-04 21:50:43 +0800

    Update 2.5.2-PIRAndLamp.ino
    注释了低电平继电器的开关问题，新手会被困扰很久（比如我）

--- 2.5.2-PIRAndLamp.ino
+++ 2.5.2-PIRAndLamp.ino
@@ -17,8 +17,8 @@ void loop() {
   while(!digitalRead(PIRpin)){}
   // 将灯打开10秒，然后关闭
   Serial.println("turn on");
-  digitalWrite(RELAYpin,HIGH);
+  digitalWrite(RELAYpin,HIGH);//继电器引脚输出高电平（打开），这个要看所使用的继电器是高电平触发还是低电平触发，如果是低电平触发的继电器需要把此处改为digitalWrite(RELAYpin,LOW)以导通继电器;
   delay(10000);  
-  digitalWrite(RELAYpin,LOW);
+  digitalWrite(RELAYpin,LOW);//同理,低电平触发的继电器此处应改为 digitalWrite(RELAYpin,HIGH);
   Serial.println("turn off");  
 }
